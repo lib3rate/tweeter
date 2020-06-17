9471ac85a6b1d8206de7891db132ff725587e467
Dmitry Petrov <e.liberate@gmail.com>
2020-06-17 19:22:33 +0000

    Initial commit with main page HTML and CSS added along with a JS functionality for updating the remaining characters count

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,68 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$('document').ready(function() {
+  
+  $("time.timeago").timeago();
+
+  const createTweetElement = function(tweet) {
+    const postedTweet = `
+      <article class="posted-tweet">
+        <header>
+          <div>
+            <img src=${tweet.user.avatars} class="user-image-small">
+            <text>${tweet.user.name}</text>
+          </div>
+          <text class="handle">${tweet.user.handle}</text>
+        </header>
+        <text class="posted-tweet-text">
+          ${tweet.content.text}
+        </text>
+        <footer>
+          <text>
+          ${$.timeago(tweet.created_at)}
+          </text>
+          <div>
+            Icons
+          </div>
+        </footer>
+      </article>
+    `
+    return postedTweet;
+  }
+
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+
+  const renderTweets = function(tweetsArray) {
+    for (let tweet of tweetsArray) {
+      const $currentTweet = createTweetElement(tweet);
+      $('#tweets-container').append($currentTweet)
+    }
+  }
+
+  renderTweets(data);
+
+});
---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $('#tweet-text').on('keyup', function() {
+    let charactersRemaining = 140 - this.value.length;
+    let characterCounter = $(this).closest('form').find('.counter')[0];
+    $(characterCounter).val(charactersRemaining);
+    if ($(characterCounter).val() < 0) {
+      $(characterCounter).addClass('negative');
+    }
+    if ($(characterCounter).val() >= 0) {
+      $(characterCounter).removeClass('negative');
+    }
+  })
+});
