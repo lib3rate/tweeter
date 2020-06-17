a176c6611fc894ded0bd9cd9036481c3192ebb68
Dmitry Petrov <e.liberate@gmail.com>
2020-06-17 23:04:27 +0000

    Added an AJAX request to add the tweet to the database

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -60,31 +60,19 @@ $('document').ready(function() {
       .then(console.log('Added to the database'))
   });
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
+  // Loading the tweets feed when opening the page
 
-  renderTweets(data);
+  const loadTweets = function() {
+    console.log('Page loaded, performing ajax call...');
+    $.ajax({
+      url: '/tweets',
+      method: 'GET'
+    })
+      .then(function (response) {
+        renderTweets(response);
+      });
+  }
+
+  loadTweets();
 
 });
